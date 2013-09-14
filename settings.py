bd3c9282b655716c8f687a812b1735406c76b6e7
Song <nosongyang@gmail.com>
2013-09-14 17:01:31 +0800

    test

--- settings.py
+++ settings.py
@@ -29,6 +29,9 @@ DATABASES = {
 # system time zone.
 TIME_ZONE = 'America/Chicago'
 
+
+
+
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
